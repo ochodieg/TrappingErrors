1abda8fc60f29b98061d4651263844d74c04c749
Ivan <the_real_iori_orochi@hotmail.com>
2020-08-28 08:13:44 -0600

    Add project files.

---
+++ TrappingErrors.vb
@@ -0,0 +1,17 @@
+﻿Option Strict On
+Option Explicit On
+Option Compare Binary
+
+Module TrappingErrors
+
+    Sub Main()
+        Dim userinput As String
+        Console.WriteLine("type a number and press enter")
+        userinput = Console.ReadLine
+
+        Console.WriteLine(userinput =)
+
+
+    End Sub
+
+End Module
