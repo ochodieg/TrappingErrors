be6da3440a9cc1a8ca41f1fe268c03cca9eb0257
Ivan <the_real_iori_orochi@hotmail.com>
2020-08-28 08:57:05 -0600

    initial commit

--- TrappingErrors.vb
+++ TrappingErrors.vb
@@ -6,10 +6,41 @@ Module TrappingErrors
 
     Sub Main()
         Dim userinput As String
-        Console.WriteLine("type a number and press enter")
-        userinput = Console.ReadLine
+        Dim userNumber As Integer
 
-        Console.WriteLine(userinput =)
+        Do
+
+            'While userinput <> "Q" or userinput <> "q"
+
+            Console.WriteLine("type a number and press enter. Enter Q to quit.")
+            userinput$ = Console.ReadLine()
+
+            'convert user input from string to integer with "cint" command
+            'hover over and press f1 to bring up explenation
+
+
+
+            Try
+
+                userNumber% = CInt(userinput$)
+                Console.WriteLine(userinput & "+ 5 is:")
+                Console.WriteLine(userNumber% + 5)
+
+
+            Catch ex As InvalidCastException
+                'ex is any variable
+                'f1 on trycatch 
+                ' catch ex as invalid exception
+                Console.WriteLine("I asked for a number, numbnuts....you entered:" & userinput)
+
+            End Try
+
+        Loop Until userinput$ = "Q" Or userinput$ = "q"
+
+        'End While
+
+        Console.ReadLine()
+
 
 
     End Sub
